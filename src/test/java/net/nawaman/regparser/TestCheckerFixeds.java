78f8d927fbefa53ad967a4234280764a3db55e82
Nawa Manusitthipol <nawa@nawaman.net>
2021-10-23 02:55:08 -0400

    Convert TestJavaChecker and TestCheckerFixeds.

---
+++ src/test/java/net/nawaman/regparser/TestCheckerFixeds.java
@@ -0,0 +1,32 @@
+package net.nawaman.regparser;
+
+import static net.nawaman.regparser.TestUtils.validate;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class TestCheckerFixeds {
+    
+    @ClassRule
+    public static TimeRecordRule timeRecordRule = TimeRecordRule.instance;
+    
+    @Test
+    public void testCheckerFixeds() {
+        var typeProvider = new PTypeProvider.Extensible();
+        typeProvider.addType("Number", RegParser.newRegParser("[0-9]*"));
+        typeProvider.addType("TestCG", new CheckerFixeds(
+                                new CheckerFixeds.Entry(1), 
+                                new CheckerFixeds.Entry(4),
+                                new CheckerFixeds.Entry("G1", 5, typeProvider.getType("Number").getTypeRef()), 
+                                new CheckerFixeds.Entry())
+                );
+        var result = typeProvider.getType("TestCG").parse("0123456789ABCDEFG");
+        validate("\n"
+                + "00 - => [   17] = <NoName>        :TestCG           = \"0123456789ABCDEFG\"\n"
+                + ". 00 => [    5] = <NoName>        :<NoType>         = \"01234\"\n"
+                + ". 01 => [   10] = G1              :Number           = \"56789\"\n"
+                + ". 02 => [   17] = <NoName>        :<NoType>         = \"ABCDEFG\"",
+                result);
+    }
+     
+}
---
+++ src/test/java/net/nawaman/regparser/TestJavaChecker.java
@@ -0,0 +1,88 @@
+package net.nawaman.regparser;
+
+import static net.nawaman.regparser.RegParser.newRegParser;
+import static net.nawaman.regparser.TestUtils.validate;
+
+import org.junit.Test;
+
+public class TestJavaChecker {
+    
+    @SuppressWarnings("serial")
+    static public class DigitChecker implements Checker {
+        static public final DigitChecker Instance = new DigitChecker();
+        
+        static public final DigitChecker getInstance() {
+            return DigitChecker.Instance;
+        }
+        
+        public DigitChecker() {
+        }
+        
+        public int getStartLengthOf(CharSequence string, int offset, PTypeProvider typeProvider) {
+            return this.getStartLengthOf(string, offset, typeProvider, null);
+        }
+        
+        public int getStartLengthOf(CharSequence string, int offset, PTypeProvider typeProvider, ParseResult result) {
+            return DigitChecker.getLengthOf(string, offset, typeProvider, result);
+        }
+        
+        // Returns the length of the detected digits.
+        static public int getLengthOf(CharSequence string, int offset, PTypeProvider typeProvider, ParseResult result) {
+            for (int i = offset; i < string.length(); i++) {
+                char c = string.charAt(i);
+                if (!((c >= '0') && (c <= '9')))
+                    return i - offset;
+            }
+            return 0;
+        }
+        
+        public Checker getOptimized() {
+            return this;
+        }
+    }
+    
+    @Test
+    public void testJavaChecker() {
+        // Point to a static method
+        var parser = newRegParser("!javaChecker(`net.nawaman.regparser.TestJavaChecker.DigitChecker->getLengthOf`)!");
+        var result = parser.parse("0123456fdgd");
+        validate("\n"
+                + "00 - => [    7] = <NoName>        :javaChecker      = \"0123456\"\n"
+                + ". 00 => [    7] = <NoName>        :javaChecker      = \"0123456\"",
+                result);
+        
+        
+        // Point to the class
+        parser = newRegParser("!javaChecker(`net.nawaman.regparser.TestJavaChecker.DigitChecker`)!");
+        result = parser.parse("012345fdgd");
+        validate("\n"
+                + "00 - => [    6] = <NoName>        :javaChecker      = \"012345\"\n"
+                + ". 00 => [    6] = <NoName>        :javaChecker      = \"012345\"", result);
+        
+        
+        // Point to the static field
+        parser = newRegParser("!javaChecker(`net.nawaman.regparser.TestJavaChecker.DigitChecker::Instance`)!");
+        result = parser.parse("01234fdgd");
+        validate("\n"
+                + "00 - => [    5] = <NoName>        :javaChecker      = \"01234\"\n"
+                + ". 00 => [    5] = <NoName>        :javaChecker      = \"01234\"",
+                result);
+        
+        
+        // Point to the factory method.
+        parser = newRegParser("!javaChecker(`net.nawaman.regparser.TestJavaChecker.DigitChecker::getInstance()`)!");
+        result = parser.parse("0123fdgd");
+        validate("\n"
+                + "00 - => [    4] = <NoName>        :javaChecker      = \"0123\"\n"
+                + ". 00 => [    4] = <NoName>        :javaChecker      = \"0123\"",
+                result);
+        
+        
+        result = parser.parse("012fdgd");
+        validate("\n"
+                + "00 - => [    3] = <NoName>        :javaChecker      = \"012\"\n"
+                + ". 00 => [    3] = <NoName>        :javaChecker      = \"012\"",
+                result);
+    }
+    
+}
