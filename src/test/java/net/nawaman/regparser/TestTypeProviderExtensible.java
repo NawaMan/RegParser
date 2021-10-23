343974f72d9e2fa8c2ca4124af0b117234019bb4
Nawa Manusitthipol <nawa@nawaman.net>
2021-10-23 02:35:14 -0400

    Convert TestTypeProviderExtensible

---
+++ src/test/java/net/nawaman/regparser/TestTypeProviderExtensible.java
@@ -0,0 +1,25 @@
+package net.nawaman.regparser;
+
+import static net.nawaman.regparser.TestUtils.validate;
+
+import org.junit.Test;
+
+public class TestTypeProviderExtensible {
+    
+    @Test
+    public void test() {
+        var typeProvider = new PTypeProvider.Extensible();
+        typeProvider.addType("Number", "[0-9]+");
+        typeProvider.addType("Plus", "!Number![:WhiteSpace:]*[:+:][:WhiteSpace:]*!Number!");
+        
+        var type   = typeProvider.getType("Plus");
+        var result = type.parse("5 + 10");
+        validate("\n"
+                + "00 - => [    6] = <NoName>        :Plus             = \"5 + 10\"\n"
+                + ". 00 => [    1] = <NoName>        :Number           = \"5\"\n"
+                + ". 01 => [    4] = <NoName>        :<NoType>         = \" + \"\n"
+                + ". 02 => [    6] = <NoName>        :Number           = \"10\"",
+                result);
+    }
+    
+}
