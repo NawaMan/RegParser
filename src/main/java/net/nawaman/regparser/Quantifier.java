dcfa4abc74ab3c42af8e88622bba263ea4c8aad2
Nawa Manusitthipol <nawa@nawaman.net>
2021-11-27 09:12:11 -0500

    Modernize Quantifier

--- src/main/java/net/nawaman/regparser/Quantifier.java
+++ src/main/java/net/nawaman/regparser/Quantifier.java
@@ -33,238 +33,222 @@ import java.util.concurrent.atomic.AtomicReference;
  * @author Nawapunth Manusitthipol (https://github.com/NawaMan)
  */
 public final class Quantifier implements Serializable {
-    
-    private static final long serialVersionUID = 1930305369240858722L;
-    
-    /** Predefine Quantifier for Zero */
-    public static final Quantifier Zero       = new Quantifier(0, 0, Possessive);
-    /** Predefine Quantifier for One */
-    public static final Quantifier One        = new Quantifier(1, 1, Possessive);
-    /** Predefine Quantifier for ZeroOrOne */
-    public static final Quantifier ZeroOrOne  = new Quantifier(0, 1, Possessive);
-    /** Predefine Quantifier for ZeroOrMore */
-    public static final Quantifier ZeroOrMore = new Quantifier(0, Possessive);
-    /** Predefine Quantifier for OneOrMore */
-    public static final Quantifier OneOrMore  = new Quantifier(1, Possessive);
-    
-    /** Predefine Quantifier for ZeroOrOne */
-    public static final Quantifier ZeroOrOne_Maximum     = new Quantifier(0, 1, Maximum);
-    /** Predefine Quantifier for ZeroOrMore */
-    public static final Quantifier ZeroOrMore_Maximum    = new Quantifier(0, Maximum);
-    /** Predefine Quantifier for OneOrMore */
-    public static final Quantifier OneOrMore_Maximum     = new Quantifier(1, Maximum);
-    /** Predefine Quantifier for ZeroOrOne */
-    public static final Quantifier ZeroOrOne_Minimum     = new Quantifier(0, 1, Minimum);
-    /** Predefine Quantifier for ZeroOrMore */
-    public static final Quantifier ZeroOrMore_Minimum    = new Quantifier(0, Minimum);
-    /** Predefine Quantifier for OneOrMore */
-    public static final Quantifier OneOrMore_Minimum     = new Quantifier(1, Minimum);
-    /** Predefine Quantifier for ZeroOrOne */
-    public static final Quantifier ZeroOrOne_Possessive  = ZeroOrOne;
-    /** Predefine Quantifier for ZeroOrMore */
-    public static final Quantifier ZeroOrMore_Possessive = ZeroOrMore;
-    /** Predefine Quantifier for OneOrMore */
-    public static final Quantifier OneOrMore_Possessive  = OneOrMore;
-    
-    /** The upper bound value for no upper bound. */
-    public static final int NO_UPPERBOUND = -1;
-    
-    /**
-     * Returns toString for the quantifier or empty string if the given value is null.
-     * 
-     * @param quantifier  the quantifier.
-     * @return            the toString for the quantifier or empty string if the given value is null.
-     */
-    public static String toString(Quantifier quantifier) {
-        return (quantifier == null) ? "" : quantifier.toString();
-    }
-    
-    private final int        lowerBound;
-    private final int        upperBound;
-    private final Greediness greediness;
-    
-    private transient final AtomicReference<String> toString = new AtomicReference<>(null);
-    
-    public Quantifier(int lowerBound, Greediness greediness) {
-        this(lowerBound, -1, greediness);
-    }
-    
-    public Quantifier(int lowerBound, int upperBound) {
-        this(lowerBound, upperBound, null);
-    }
-    
-    public Quantifier(int lowerBound, int upperBound, Greediness greediness) {
-        if ((lowerBound < 0) || ((upperBound >= 0) && (lowerBound > upperBound))) {
-            var errMsg = format("Invalid qualifier: lowerBound=[%d] upperBound=[%d]", lowerBound, upperBound);
-            throw new IllegalArgumentException(errMsg);
-        }
-        
-        if (upperBound < 0)
-            upperBound = -1;
-        
-        this.lowerBound = lowerBound;
-        this.upperBound = upperBound;
-        this.greediness = (greediness != null) ? greediness : Possessive;
-    }
-    
-    
-    /** Checks if this Quantifier is a one and possessive */
-    public boolean isOne_Possessive() {
-        return this.isOne()
-            && this.isPossessive();
-    }
-    
-    /** Checks if this Quantifier is a zero */
-    public boolean isZero() {
-        return (this.lowerBound == 0)
-            && (this.upperBound == 0);
-    }
-    
-    /** Checks if this Quantifier is a one */
-    public boolean isOne() {
-        return (this.lowerBound == 1)
-            && (this.upperBound == 1);
-    }
-    
-    /** Checks if this Quantifier is a zero or one */
-    public boolean isZeroOrOne() {
-        return (this.lowerBound == 0)
-            && (this.upperBound == 1);
-    }
-    
-    /** Checks if this Quantifier is a zero or more */
-    public boolean isZeroOrMore() {
-        return (this.lowerBound == 0)
-            && (this.upperBound == -1);
-    }
-    
-    /** Checks if this Quantifier is a one or more */
-    public boolean isOneOrMore() {
-        return (this.lowerBound == 1)
-            && (this.upperBound == -1);
-    }
-    
-    /** Returns the lower bound */
-    public int lowerBound() {
-        return this.lowerBound;
-    }
-    
-    /** Returns the upper bound (-1 for unlimited) */
-    public int upperBound() {
-        return this.upperBound;
-    }
-    
-    /** Checks if this quantifier has no upper bound */
-    public boolean hasNoUpperBound() {
-        return this.upperBound == NO_UPPERBOUND;
-    }
-    
-    /** Checks if this quantifier has no upper bound */
-    public boolean hasUpperBound() {
-        return this.upperBound != NO_UPPERBOUND;
-    }
-    
-    /** Returns how greedy this quali */
-    public Greediness greediness() {
-        return this.greediness;
-    }
-    
-    /** Checks if this is a maximum greediness */
-    public boolean isMaximum() {
-        return this.greediness == Greediness.Maximum;
-    }
-    
-    /** Checks if this is a minimum greediness */
-    public boolean isMinimum() {
-        return this.greediness == Greediness.Minimum;
-    }
-    
-    /** Checks if this is a possessive greediness */
-    public boolean isPossessive() {
-        return this.greediness == Greediness.Possessive;
-    }
-    
-    /** @return  a quantifier with this lower/upper bound but with possessive greediness. */
-    public Quantifier withPossessive() {
-        return (greediness == Possessive)
-                ? this
-                : new Quantifier(lowerBound, upperBound, Possessive);
-    }
-    
-    /** @return  a quantifier with this lower/upper bound but with maximum greediness. */
-    public Quantifier withMaximum() {
-        return (greediness == Maximum)
-                ? this
-                : new Quantifier(lowerBound, upperBound, Maximum);
-    }
-    
-    /** @return  a quantifier with this lower/upper bound but with minimum greediness. */
-    public Quantifier withMinimum() {
-        return (greediness == Minimum)
-                ? this
-                : new Quantifier(lowerBound, upperBound, Minimum);
-    }
-    
-    /** Returns the string representation of the qualifier */
-    @Override
-    public String toString() {
-        return toString.updateAndGet(oldValue -> {
-            if (oldValue != null) {
-                return oldValue;
-            }
-            
-            if (this.isZeroOrOne()) {
-                return "?" + this.greediness.sign();
-            }
-            if (this.isZeroOrMore()) {
-                return "*" + this.greediness.sign();
-            }
-            if (this.isOneOrMore()) {
-                return "+" + this.greediness.sign();
-            }
-            
-            if (this.isZero()) {
-                return "{0}";
-            }
-            if (this.isOne()) {
-                return "" + this.greediness.sign();
-            }
-            
-            if (this.lowerBound == this.upperBound) {
-                return "{" + this.lowerBound + "}" + this.greediness.sign();
-            }
-            if (this.lowerBound == 0) {
-                return "{," + this.upperBound + "}" + this.greediness.sign();
-            }
-            if (this.upperBound == -1) {
-                return "{" + this.lowerBound + ",}" + this.greediness.sign();
-            }
-            
-            return "{" + this.lowerBound + "," + this.upperBound + "}" + this.greediness.sign();
-        });
-    }
-    
-    @Override
-    public int hashCode() {
-        return Objects.hash(greediness, lowerBound, toString, upperBound);
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        
-        var other = (Quantifier)obj;
-        return greediness == other.greediness
-            && lowerBound == other.lowerBound
-            && upperBound == other.upperBound;
-    }
-    
+	
+	private static final long serialVersionUID = 1930305369240858722L;
+	
+	/** Predefine Quantifier for Zero */
+	public static final Quantifier Zero       = new Quantifier(0, 0, Possessive);
+	/** Predefine Quantifier for One */
+	public static final Quantifier One        = new Quantifier(1, 1, Possessive);
+	/** Predefine Quantifier for ZeroOrOne */
+	public static final Quantifier ZeroOrOne  = new Quantifier(0, 1, Possessive);
+	/** Predefine Quantifier for ZeroOrMore */
+	public static final Quantifier ZeroOrMore = new Quantifier(0, Possessive);
+	/** Predefine Quantifier for OneOrMore */
+	public static final Quantifier OneOrMore  = new Quantifier(1, Possessive);
+	
+	/** Predefine Quantifier for ZeroOrOne */
+	public static final Quantifier ZeroOrOne_Maximum     = new Quantifier(0, 1, Maximum);
+	/** Predefine Quantifier for ZeroOrMore */
+	public static final Quantifier ZeroOrMore_Maximum    = new Quantifier(0, Maximum);
+	/** Predefine Quantifier for OneOrMore */
+	public static final Quantifier OneOrMore_Maximum     = new Quantifier(1, Maximum);
+	/** Predefine Quantifier for ZeroOrOne */
+	public static final Quantifier ZeroOrOne_Minimum     = new Quantifier(0, 1, Minimum);
+	/** Predefine Quantifier for ZeroOrMore */
+	public static final Quantifier ZeroOrMore_Minimum    = new Quantifier(0, Minimum);
+	/** Predefine Quantifier for OneOrMore */
+	public static final Quantifier OneOrMore_Minimum     = new Quantifier(1, Minimum);
+	/** Predefine Quantifier for ZeroOrOne */
+	public static final Quantifier ZeroOrOne_Possessive  = ZeroOrOne;
+	/** Predefine Quantifier for ZeroOrMore */
+	public static final Quantifier ZeroOrMore_Possessive = ZeroOrMore;
+	/** Predefine Quantifier for OneOrMore */
+	public static final Quantifier OneOrMore_Possessive  = OneOrMore;
+	
+	/** The upper bound value for no upper bound. */
+	public static final int NO_UPPERBOUND = -1;
+	
+	/**
+	 * Returns toString for the quantifier or empty string if the given value is null.
+	 * 
+	 * @param quantifier  the quantifier.
+	 * @return            the toString for the quantifier or empty string if the given value is null.
+	 */
+	public static String toString(Quantifier quantifier) {
+		return (quantifier == null) ? "" : quantifier.toString();
+	}
+	
+	private final int        lowerBound;
+	private final int        upperBound;
+	private final Greediness greediness;
+	
+	private transient final AtomicReference<String> toString = new AtomicReference<>(null);
+	
+	public Quantifier(int lowerBound, Greediness greediness) {
+		this(lowerBound, -1, greediness);
+	}
+	
+	public Quantifier(int lowerBound, int upperBound) {
+		this(lowerBound, upperBound, null);
+	}
+	
+	public Quantifier(int lowerBound, int upperBound, Greediness greediness) {
+		if ((lowerBound < 0) || ((upperBound >= 0) && (lowerBound > upperBound))) {
+			var errMsg = format("Invalid qualifier: lowerBound=[%d] upperBound=[%d]", lowerBound, upperBound);
+			throw new IllegalArgumentException(errMsg);
+		}
+		
+		if (upperBound < 0) {
+			upperBound = -1;
+		}
+		
+		this.lowerBound = lowerBound;
+		this.upperBound = upperBound;
+		this.greediness = (greediness != null) ? greediness : Possessive;
+	}
+	
+	
+	/** Checks if this Quantifier is a one and possessive */
+	public boolean isOne_Possessive() {
+		return isOne() && isPossessive();
+	}
+	
+	/** Checks if this Quantifier is a zero */
+	public boolean isZero() {
+		return (lowerBound == 0) && (upperBound == 0);
+	}
+	
+	/** Checks if this Quantifier is a one */
+	public boolean isOne() {
+		return (lowerBound == 1) && (upperBound == 1);
+	}
+	
+	/** Checks if this Quantifier is a zero or one */
+	public boolean isZeroOrOne() {
+		return (lowerBound == 0) && (upperBound == 1);
+	}
+	
+	/** Checks if this Quantifier is a zero or more */
+	public boolean isZeroOrMore() {
+		return (lowerBound == 0) && (upperBound == -1);
+	}
+	
+	/** Checks if this Quantifier is a one or more */
+	public boolean isOneOrMore() {
+		return (lowerBound == 1) && (upperBound == -1);
+	}
+	
+	/** Returns the lower bound */
+	public int lowerBound() {
+		return lowerBound;
+	}
+	
+	/** Returns the upper bound (-1 for unlimited) */
+	public int upperBound() {
+		return upperBound;
+	}
+	
+	/** Checks if this quantifier has no upper bound */
+	public boolean hasNoUpperBound() {
+		return upperBound == NO_UPPERBOUND;
+	}
+	
+	/** Checks if this quantifier has no upper bound */
+	public boolean hasUpperBound() {
+		return upperBound != NO_UPPERBOUND;
+	}
+	
+	/** Returns how greedy this qualifier */
+	public Greediness greediness() {
+		return greediness;
+	}
+	
+	/** Checks if this is a maximum greediness */
+	public boolean isMaximum() {
+		return greediness == Greediness.Maximum;
+	}
+	
+	/** Checks if this is a minimum greediness */
+	public boolean isMinimum() {
+		return greediness == Greediness.Minimum;
+	}
+	
+	/** Checks if this is a possessive greediness */
+	public boolean isPossessive() {
+		return greediness == Greediness.Possessive;
+	}
+	
+	/** @return  a quantifier with this lower/upper bound but with possessive greediness. */
+	public Quantifier withPossessive() {
+		return (greediness == Possessive) ? this : new Quantifier(lowerBound, upperBound, Possessive);
+	}
+	
+	/** @return  a quantifier with this lower/upper bound but with maximum greediness. */
+	public Quantifier withMaximum() {
+		return (greediness == Maximum) ? this : new Quantifier(lowerBound, upperBound, Maximum);
+	}
+	
+	/** @return  a quantifier with this lower/upper bound but with minimum greediness. */
+	public Quantifier withMinimum() {
+		return (greediness == Minimum) ? this : new Quantifier(lowerBound, upperBound, Minimum);
+	}
+	
+	/** Returns the string representation of the qualifier */
+	@Override
+	public String toString() {
+		return toString.updateAndGet(oldValue -> {
+			if (oldValue != null)
+				return oldValue;
+			
+			if (isZeroOrOne())
+				return "?" + greediness.sign();
+			
+			if (isZeroOrMore())
+				return "*" + greediness.sign();
+			
+			if (isOneOrMore())
+				return "+" + greediness.sign();
+			
+			if (isZero())
+				return "{0}";
+			
+			if (isOne())
+				return "" + greediness.sign();
+			
+			if (lowerBound == upperBound)
+				return "{" + lowerBound + "}" + greediness.sign();
+			
+			if (lowerBound == 0)
+				return "{," + upperBound + "}" + greediness.sign();
+			
+			if (upperBound == -1)
+				return "{" + lowerBound + ",}" + greediness.sign();
+			
+			return "{" + lowerBound + "," + upperBound + "}" + greediness.sign();
+		});
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(greediness, lowerBound, toString, upperBound);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		var other = (Quantifier)obj;
+		return greediness == other.greediness
+		    && lowerBound == other.lowerBound
+		    && upperBound == other.upperBound;
+	}
+	
 }
