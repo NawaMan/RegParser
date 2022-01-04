ecaf3f01017f88471a80a36ac24025db42817f08
Nawa Manusitthipol <nawa@nawaman.net>
2022-01-03 22:37:35 -0500

    Update email address in the licence.

--- src/main/java/net/nawaman/regparser/types/StringLiteralParserType.java
+++ src/main/java/net/nawaman/regparser/types/StringLiteralParserType.java
@@ -8,7 +8,7 @@
  * The project is a free software; you can redistribute it and/or modify it under the SIMILAR terms of the GNU General
  * Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
  * You are only required to inform me about your modification and redistribution as or as part of commercial software
- * package. You can inform me via nawaman<at>gmail<dot>com.
+ * package. You can inform me via nawa<at>nawaman<dot>net.
  * 
  * The project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
